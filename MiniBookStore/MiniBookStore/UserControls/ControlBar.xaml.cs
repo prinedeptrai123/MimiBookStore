a4a3c9d0ff292f3533221ba42800c5df7765c6fb
Le Tuong Qui <qui021098@gmail.com>
2019-01-06 11:42:51 +0700

    controlbar

---
+++ MiniBookStore/MiniBookStore/UserControls/ControlBar.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MiniBookStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MiniBookStore.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ControlBar.xaml
+    /// </summary>
+    public partial class ControlBar : UserControl
+    {
+        public ControlBarVM viewmodel { get; set; }
+        
+        public ControlBar()
+        {
+            InitializeComponent();
+            this.DataContext = viewmodel = new ControlBarVM();
+        }
+    }
+}
