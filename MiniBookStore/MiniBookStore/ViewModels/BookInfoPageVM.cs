6910601839381784b03f4090ad32838d409a9460
Le Tuong Qui <qui021098@gmail.com>
2019-01-07 21:48:13 +0700

    fixx

--- MiniBookStore/MiniBookStore/ViewModels/BookInfoPageVM.cs
+++ MiniBookStore/MiniBookStore/ViewModels/BookInfoPageVM.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MiniBookStore.ViewModels
 {
     public class BookInfoPageVM:BaseViewModel
     {
+        #region properties binding
 
+        #endregion
+
+        #region command binding
+
+        public ICommand addNewBookCommand { get; set; }
+
+        #endregion
+
+        public BookInfoPageVM()
+        {
+            addNewBookCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                //Khởi tạo
+                AddNewBookWindow wd = new AddNewBookWindow();
+                wd.ShowDialog();
+               
+            }
+               );
+        }
     }
 }
